c05ec4cc79412d91901c93bcc75b604db214ce1e
Ant-Pearl <carlosalvatici@gmail.com>
2022-10-09 10:08:59 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<header class="bg-black py-5">
+		<h1 class="text-white">Pho Ha Noi</h1>
+	</header>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		
+	}
+
+	header {
+		background: black;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
